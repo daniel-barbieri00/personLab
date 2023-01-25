e6d70257f07a231544f43db604480ee833a3b9b7
Daniel <51567745+daniel-barbieri00@users.noreply.github.com>
2023-01-25 09:46:36 -0700

    Add files via upload

---
+++ personClassLab/Person.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personClassLab
+{
+    internal class Person
+    {
+        private int personId;
+        public string firstName;
+        private string lastName;
+        private string favouriteColor;
+        public int age;
+        private bool isWorking;
+
+        public Person(int personId, string firstName, string lastName, string favouriteColor, int age, bool isWorking)
+        {
+            this.personId = personId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.favouriteColor = favouriteColor;
+            this.age = age;
+            this.isWorking = isWorking;
+        }
+
+        public void DisplayPersonInfo()
+        {
+            string personInfo = ("PersonId: " + this.personId
+                + "\nFirst Name: " + this.firstName
+                + "\nLast Name: " + this.lastName
+                + "\nFavouriteColor: " + this.favouriteColor
+                + "\nAge: " + this.age
+                + "\nIs Working: " + this.isWorking);
+
+            Console.WriteLine(personInfo);
+        }
+
+        public void DisplayFavouriteColor()
+        {
+            Console.WriteLine(this.firstName + "s Favourite Color: " + this.favouriteColor);
+        }
+
+        public void ChangeFavColor()
+        {   
+            this.favouriteColor = "White";
+
+            Console.WriteLine(this.firstName + "s Favourite Color: " + this.favouriteColor);
+        }
+
+        public void AgeInTenYears() 
+        {
+            int age = this.age;
+            int futureAge = age + 10;
+
+            Console.WriteLine(this.firstName + " " + this.lastName + " Age in 10 years is: " + futureAge);
+        }
+
+        public void PersonInfoToString()
+        {
+            Console.WriteLine(this.personId + ", "
+                + this.firstName  + ", " 
+                + this.lastName + ", " 
+                + this.age + ", " 
+                + this.favouriteColor + ", " 
+                + this.isWorking);
+        }
+        public void PersonInfoSentence()
+        {
+            // 2: Gina James's favorite colour is Green
+            Console.WriteLine(this.personId + ":" + this.firstName + " " + this.lastName + "'s favorite colour is " + this.favouriteColor);
+        }
+    }
+}
+
+
---
+++ personClassLab/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace personClassLab
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Creation of Persons
+            Person ianB = new Person(1, "Ian", "Brooks", "Red", 30, true);
+            Person ginaJ = new Person(2, "Gina", "James", "Green", 18, false);
+            Person mikeB = new Person(3, "Mike", "Briscoe", "Blue", 45, true);
+            Person maryB = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+
+            //creation of list of people
+            Person[] people = { ianB, ginaJ, mikeB, maryB };
+
+            //creation of lowest age variable for looping
+            //set to 200 because no one is 200 years old so I can start the search for the lowest with < 
+            int lowestAge = 200;
+            Person youngestPerson = null;
+
+            foreach (Person x in people)
+            {
+                if (x.age < lowestAge)
+                {
+                    lowestAge = x.age;
+                    youngestPerson = x;
+                }
+            }
+
+            //creation of variables to find oldest person
+            //set to 0 for the same reasons lowestAge was get to 200 but reversed
+            int highestAge = 0;
+            Person oldestPerson = null;
+
+            foreach (Person x in people)
+            {
+                if (x.age > highestAge)
+                {
+                    highestAge = x.age;
+                    oldestPerson = x;
+                }
+            }
+
+            //Creation of variables to find average age
+            float averageAge = 0;
+            float totalAge = 0;
+            int amountOfPeople = 0;
+
+            //Loop to Find average age and store it into variable
+            foreach (Person x in people)
+            {
+                totalAge = x.age + totalAge;
+                amountOfPeople = amountOfPeople + 1;
+                averageAge = totalAge / amountOfPeople;
+            }
+
+            //Calling to get correct order to match output on assignment
+            ginaJ.PersonInfoSentence();
+            mikeB.DisplayPersonInfo();
+
+            ianB.DisplayFavouriteColor();
+            ianB.ChangeFavColor();
+
+            maryB.AgeInTenYears();
+
+
+            Console.WriteLine("Average age is: " + averageAge);
+            Console.WriteLine("The youngest person is: " + youngestPerson.firstName);
+            Console.WriteLine("The oldest person is: " + oldestPerson.firstName);
+
+            mikeB.DisplayPersonInfo();
+
+            ianB.PersonInfoToString();
+        }
+    }
+}
